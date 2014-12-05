2bb1680587b92d5b9d40271b46af9a8748564460
Alexey Balchunas <bleshik@gmail.com>
2014-12-05 13:56:37 +0700

    added missing files from angular-seed

--- web-app/karma.conf.js
+++ web-app/karma.conf.js
@@ -4,9 +4,9 @@ module.exports = function(config){
     basePath : './',
 
     files : [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
       'app/view*/**/*.js'
     ],
@@ -18,8 +18,6 @@ module.exports = function(config){
     browsers : ['PhantomJS'],
 
     plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
             'karma-phantomjs-launcher',
             'karma-jasmine',
             'karma-junit-reporter'
