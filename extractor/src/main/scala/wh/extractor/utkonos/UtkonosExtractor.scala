bc9102ca7e3c4f9c390dce4ca8e9640dea5f89ab
Alexey Balchunas <bleshik@gmail.com>
2015-02-11 22:46:34 +0700

    Utkonos quantity and price fixes

--- extractor/src/main/scala/wh/extractor/utkonos/UtkonosExtractor.scala
+++ extractor/src/main/scala/wh/extractor/utkonos/UtkonosExtractor.scala
@@ -15,14 +15,25 @@ class UtkonosExtractor extends AbstractHtmlUnitExtractor {
 
   private def extractFromCategory(page: HtmlPage, category: Category): Iterator[ExtractedEntry] = {
     page.getBody.getElementsByAttribute("div", "class", "goods_container goods_view_box").asScala.headOption.asInstanceOf[Option[HtmlDivision]].map { goods =>
-      goods.getElementsByAttribute("div", "class", "goods_view").asScala.asInstanceOf[mutable.Buffer[HtmlDivision]].flatMap { entry =>
+      List("goods_view has_weight", "goods_view").flatMap(cl => goods.getElementsByAttribute("div", "class", cl).asScala.asInstanceOf[mutable.Buffer[HtmlDivision]].flatMap { entry =>
         extractEntry(
           "Утконос",
           cleanUpName(entry.getElementsByTagName("a").asScala.head.getTextContent),
-          (BigDecimal(entry.getOneHtmlElementByAttribute("input", "name", "price").asInstanceOf[HtmlInput].getValueAttribute) * 100).toLong,
+          (BigDecimal(
+            entry.getElementsByAttribute("div", "class", "price color_black")
+              .asScala
+              .headOption
+              .asInstanceOf[Option[HtmlDivision]]
+              .map(price => price.getChildNodes.get(0).getTextContent)
+              .getOrElse(
+                entry.getOneHtmlElementByAttribute("div", "class", "price color_action").asInstanceOf[HtmlDivision]
+                  .getChildNodes.get(1)
+                  .getTextContent
+              ).replaceAll("\\s+", "").replace(",", ".")
+          ) * 100).toLong,
           category,
           entry.getElementsByTagName("img").get(0))
-      }.iterator ++ page.getBody.getElementsByAttribute("div", "class", "el_paginate").asScala.headOption.asInstanceOf[Option[HtmlDivision]].map { pagination =>
+      }).iterator ++ page.getBody.getElementsByAttribute("div", "class", "el_paginate").asScala.headOption.asInstanceOf[Option[HtmlDivision]].map { pagination =>
         pagination.getElementsByTagName("a").asScala.lastOption.asInstanceOf[Option[HtmlAnchor]].map { lastLink =>
           if (cleanUpName(lastLink.getTextContent).equals("Вперед"))
             click(lastLink).map(extractFromCategory(_, category)).getOrElse(Iterator.empty)
