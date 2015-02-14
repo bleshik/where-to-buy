15cfb82c422c86c748a1b73ec67e7daaea1d760b
Alexey Balchunas <bleshik@gmail.com>
2015-02-14 13:21:25 +0700

    Increased etcd client timeout. Also, added even more logs.

--- backend/api/src/main/scala/wh/infrastructure/etcd/SyncEtcdClient.scala
+++ backend/api/src/main/scala/wh/infrastructure/etcd/SyncEtcdClient.scala
@@ -6,14 +6,14 @@ import net.nikore.etcd.EtcdClient
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-class SyncEtcdClient(val etcdClient: EtcdClient, val duration: Duration = Duration(1, "s")) extends LazyLogging {
+class SyncEtcdClient(val etcdClient: EtcdClient, val duration: Duration = Duration(5, "s")) extends LazyLogging {
 
   def list(dir: String, defaultValues: List[String]): List[String] = {
     try {
       Await.result(etcdClient.listDir(dir), duration).node.nodes.map(o => o.map(e => e.key)).getOrElse(defaultValues)
     } catch {
       case e: Exception =>
-        logger.error("Coudln't get a value from etcd", e)
+        logger.error(s"Couldn't get a list $dir from etcd", e)
         defaultValues
     }
   }
@@ -23,7 +23,7 @@ class SyncEtcdClient(val etcdClient: EtcdClient, val duration: Duration = Durati
       Await.result(etcdClient.getKey(key), duration).node.value.getOrElse(defaultValue)
     } catch {
       case e: Exception =>
-        logger.error("Coudln't get a value from etcd", e)
+        logger.error(s"Couldn't get a value $key from etcd", e)
         defaultValue
     }
   }
