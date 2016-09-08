0b8bf30077084031b570fc68d804152b77d804b0
Alexey Balchunas <bleshik@gmail.com>
2016-09-08 22:20:03 +0300

    Extractor: create lambda if update fails

--- backend/extractor/build-and-deploy.sh
+++ backend/extractor/build-and-deploy.sh
@@ -4,4 +4,4 @@ if [ -z "$AWS_REGION" ] ; then
 fi
 cd "$(dirname "$0")"
 (echo "accessKey = $AWS_ACCESS_KEY_ID" ; echo "secretKey = $AWS_SECRET_ACCESS_KEY") > ./src/main/resources/AwsCredentials.properties
-. ../../pull_cache && sbt clean && sbt updateLambda && . ../../cache_deps
+. ../../pull_cache && sbt clean && (sbt updateLambda || sbt createLambda) && . ../../cache_deps
